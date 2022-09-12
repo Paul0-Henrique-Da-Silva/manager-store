6102ded1ae7e1c22c14d391dc8d0554e32382893
Paul0-Henrique-Da-Silva <ph46163835@gmail.com>
2022-09-12 19:56:57 -0300

    update app

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const routes = require('./routes/index');
 
 const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use(routes);
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
