3acd03eb82323a0f891128d5cf73abd6a87d0f1a
Paul0-Henrique-Da-Silva <ph46163835@gmail.com>
2022-09-12 19:36:47 -0300

    service/product

---
+++ src/services/productsService.js
@@ -0,0 +1,8 @@
+const productsModel = require('../models/productsModel');
+
+const getAll = async () => {
+  const result = await productsModel.getAll();
+  return result;
+};
+
+module.exports = getAll;
